fd9929b15617081e2f0268616fa09cfa538a2364
thomasalakopsa <tz.alakopsa@hotmail.com>
2020-03-11 17:21:41 +0100

    first part of the math class done

---
+++ MyMath.cpp
@@ -0,0 +1,6 @@
+#include "MyMath.h"
+
+ RowVector MyMath::add2RowVectors(RowVector rvec1, RowVector rvec2)
+ {
+     return rvec1;
+ }
---
+++ MyMath.h
@@ -0,0 +1,12 @@
+#ifndef MYMATH_H
+#define MYMATH_H
+
+#include "RowVector.h"
+
+class MyMath
+{
+    public:
+        RowVector add2RowVectors(RowVector rvec1, RowVector rvec2);
+};
+
+#endif
